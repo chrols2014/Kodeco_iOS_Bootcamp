e193f38218842991dfc4de64690ebec0fddf02c9
Chris Olsen <chrols@me.com>
2023-08-31 21:32:10 -0400

    Initial Commit

---
+++ Kodeco_Homework/Kodeco_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kodeco_HomeworkApp.swift
+//  Kodeco_Homework
+//
+//  Created by Chris Olsen on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct Kodeco_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
