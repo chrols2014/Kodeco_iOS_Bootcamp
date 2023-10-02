a792e3bad612e2cb1f579d9d6d71638cbd1c83d6
Chris Olsen <chrols@me.com>
2023-10-02 12:17:20 -0400

    Setup Task class and Struct. Started building TaskRowView and styling it

---
+++ week05/TaskList/TaskList/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  TaskList
+//
+//  Created by Chris Olsen on 2023-10-02.
+//
+
+import Foundation
---
+++ week05/TaskList/TaskList/TaskRowView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskRowView.swift
+//  TaskList
+//
+//  Created by Chris Olsen on 2023-10-02.
+//
+
+import SwiftUI
+
+struct TaskRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskRowView()
+}
