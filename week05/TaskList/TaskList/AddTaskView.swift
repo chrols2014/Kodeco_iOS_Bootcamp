bc218394a357b3e8ad0b9b9916134071776bca18
Chris Olsen <chrols@me.com>
2023-10-02 14:22:33 -0400

    Add works, testing if environment object is better implementation

---
+++ week05/TaskList/TaskList/AddTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTaskView.swift
+//  TaskList
+//
+//  Created by Chris Olsen on 2023-10-02.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
---
+++ week05/TaskList/TaskList/TaskDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskDetailView.swift
+//  TaskList
+//
+//  Created by Chris Olsen on 2023-10-02.
+//
+
+import SwiftUI
+
+struct TaskDetailView: View {
+    var body: some View {
+      Form {
+        
+      }
+    }
+}
+
+#Preview {
+    TaskDetailView()
+}
