2e4b5a86c396742089ae02f3cf61a6efbe728652
Chris Olsen <chrols@me.com>
2023-10-30 13:28:34 -0400

    added project file for week9

---
+++ week09/Homework9/Homework9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework9
+//
+//  Created by Chris Olsen on 2023-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
