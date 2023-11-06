56439348f59eab023f1efcf1d607afe4ef3712c6
Chris Olsen <chrols@me.com>
2023-11-06 17:30:30 -0500

    added fetch function, all working

--- week10/Homework10.playground/Contents.swift
+++ week10/Homework10.playground/Contents.swift
@@ -1,46 +1,55 @@
 import Foundation
 
+
+fetchMeSomeCatFacts(numberOfFactsToGet: 10)
+
+
+
+// MARK: -CatFact Model for decoding
 struct CatFact: Codable {
   let fact: String
   let length: Int32
 }
 
 
+// MARK: -AsyncSequence
 struct CatFactSequence: AsyncSequence {
   typealias AsyncIterator = CatFactIterator
   typealias Element = CatFact
-
+  
   let requestedIterations: Int
   
   func makeAsyncIterator() -> CatFactIterator {
     return CatFactIterator(requestedIterations)
   }
-
 }
 
+
+// MARK: - Iterator
 struct CatFactIterator: AsyncIteratorProtocol {
   typealias Element = CatFact
   var currentIteration: Int
   var requestedIterations: Int
   var currentCatFact: CatFact?
-
+  
   init(_ requestedIterations: Int) {
     self.currentIteration = 0
     self.requestedIterations = requestedIterations
   }
-
+  
   mutating func next() async throws -> CatFact? {
     guard currentIteration <= requestedIterations - 1 else { return nil }
-    try await Task.sleep(until: .now + .seconds(1), clock: .continuous)
     try await currentCatFact = fetchData()
+    try await Task.sleep(until: .now + .seconds(1), clock: .continuous)
     defer {
       currentIteration += 1
     }
     return currentCatFact
   }
-
 }
 
+
+// MARK: - APICall
 func fetchData() async throws -> CatFact {
   var catFact: CatFact
   guard let url = URL(string: "https://catfact.ninja/fact") else {
@@ -49,10 +58,8 @@ func fetchData() async throws -> CatFact {
   
   let (data, response) = try await URLSession.shared.data(from: url)
   
-  
   guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
     throw CustomErrors.invalidAPIResponse
-    
   }
   
   do {
@@ -61,21 +68,27 @@ func fetchData() async throws -> CatFact {
   } catch {
     throw CustomErrors.invalidAPIData
   }
-  
-  
-  
-  
-  
   return catFact
 }
-Task {
-  for try await item in CatFactSequence(requestedIterations: 10) {
-    print(item.fact)
+
+
+// MARK: - Cat Fact Getter Function
+func fetchMeSomeCatFacts(numberOfFactsToGet: Int) {
+  if numberOfFactsToGet <= 0 {
+    print("~ Enter in a number to see the cat facts! 😿 ~")
   }
-  
+  else {
+    print("~ Getting those cat facts! 😸 ~")
+    Task {
+      for try await item in CatFactSequence(requestedIterations: numberOfFactsToGet) {
+        print("~ \(item.fact) ~")
+      }
+    }
+  }
 }
 
 
+// MARK: - Custom Errors
 enum CustomErrors: Error {
   case invalidAPIURL
   case invalidAPIResponse
