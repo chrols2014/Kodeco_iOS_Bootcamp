3798683e7a86f94fc94e4ea56149c46ea1f3a262
Chris Olsen <chrols@me.com>
2023-11-27 16:47:49 -0500

    added homescreen view and subviews, fixed NavigationDestination

---
+++ Capstone Project/Mixology/Mixology/Views/DrinkCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  DrinkCarouselView.swift
+//  Mixology
+//
+//  Created by Chris Olsen on 2023-11-27.
+//
+
+import SwiftUI
+
+struct DrinkCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrinkCarouselView()
+}
---
+++ Capstone Project/Mixology/Mixology/Views/HomeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreenView.swift
+//  Mixology
+//
+//  Created by Chris Olsen on 2023-11-27.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreenView()
+}
