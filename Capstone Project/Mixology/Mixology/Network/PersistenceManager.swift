82fc0cfd31e0921d704480de258387cdd573031e
Chris Olsen <chrols@me.com>
2023-11-30 16:39:33 -0500

    added persistnace of favourites, need to save images locally

---
+++ Capstone Project/Mixology/Mixology/Network/PersistenceManager.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceManager.swift
+//  Mixology
+//
+//  Created by Chris Olsen on 2023-11-30.
+//
+
+import Foundation
---
+++ Capstone Project/Mixology/Mixology/Views/FavouritesScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FavouritesScreen.swift
+//  Mixology
+//
+//  Created by Chris Olsen on 2023-11-30.
+//
+
+import SwiftUI
+
+struct FavouritesScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouritesScreen()
+}
