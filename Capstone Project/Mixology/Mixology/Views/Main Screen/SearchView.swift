7f437320ae696c107269386e5dcbadeb6b976eed
Chris Olsen <chrols@me.com>
2023-12-01 15:21:38 -0500

    trying to refactor async code

---
+++ Capstone Project/Mixology/Mixology/Views/Main Screen/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Mixology
+//
+//  Created by Chris Olsen on 2023-12-01.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
